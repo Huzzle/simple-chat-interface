b90467fd5bd9b18011790e0427f4c004ecdf5b46
Alexander Ryabtsev <arabtsau@klika-tech.com>
2019-12-17 16:19:16 +0300

    Implement chat footer

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { TextField } from '../../ui-kit/TextField'
+import { Button } from '../../ui-kit/Button'
 
 import './Chat.css'
 
 export const Chat = () => { 
+  const [ message, setMessage ] = useState('')
+
   return (
     <div className="Chat">
       <div className="Chat__Messages">
         Messages List
       </div>
       <div className="Chat__Footer">
-        Footer
+        <div className="Chat__MessageInput">
+          <TextField
+            placeholder='Message'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <div className="Chat__SendButton">
+          <Button onClick={() => {}} disabled={message === ''}>Send</Button>
+        </div>
       </div>
     </div>
   )
