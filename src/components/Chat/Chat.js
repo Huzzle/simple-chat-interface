71363dfb8986842349ef2052d5f5dee094c4d555
Alexander Ryabtsev <arabtsau@klika-tech.com>
2019-12-17 15:49:11 +0300

    Initial layout

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Chat.css'
+
+export const Chat = () => { 
+  return (
+    <div className="Chat">
+      <div className="Chat__Messages">
+        Messages List
+      </div>
+      <div className="Chat__Footer">
+        Footer
+      </div>
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'modern-css-reset'
+
+import { Chat } from './components/Chat'
+
+import './index.css'
+
+ReactDOM.render(
+  <Chat />,
+  document.getElementById('root')
+)
